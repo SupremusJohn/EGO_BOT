d97d3ff2540bbb3ce0ea6ee773ad3dfc3126ab40
kurameshi natsuki <111725926+kurameshinatsuki@users.noreply.github.com>
2025-07-01 22:50:06 +0000

    Update shinobi_cards.js

--- commandes/shinobi_cards.js
+++ commandes/shinobi_cards.js
@@ -1,1 +1,44 @@
-.
+module.exports = {
+    C: [
+        "https://i.ibb.co/67WJg9mn/IMG-20250701-WA0157.jpg",
+        "https://i.ibb.co/QvDV2Dxb/IMG-20250701-WA0156.jpg",
+        "https://i.ibb.co/fdqGxTw6/IMG-20250701-WA0155.jpg",
+        "https://i.ibb.co/vMnFJ23/IMG-20250701-WA0154.jpg",
+        "https://i.ibb.co/svM4J6mc/IMG-20250701-WA0153.jpg",
+        "https://i.ibb.co/d4QcD4Td/IMG-20250701-WA0150.jpg",
+        "https://i.ibb.co/JRJ8ZC28/IMG-20250701-WA0149.jpg",
+        "https://i.ibb.co/PsWK7h1F/IMG-20250701-WA0148.jpg",
+        "https://i.ibb.co/ksv16MVz/IMG-20250701-WA0147.jpg",
+        "https://i.ibb.co/WNVYS3t6/IMG-20250701-WA0145.jpg",
+        "https://i.ibb.co/DHMxsZJt/IMG-20250701-WA0138.jpg",
+        "https://i.ibb.co/qL60Cf3X/IMG-20250701-WA0136.jpg",
+        "https://i.ibb.co/7txyyPCL/IMG-20250701-WA0134.jpg",
+        "https://i.ibb.co/d0Tdv6rf/IMG-20250701-WA0133.jpg",
+        "https://i.ibb.co/6Rg4rWR9/IMG-20250701-WA0119.jpg",
+        "https://i.ibb.co/zVD6JttJ/IMG-20250701-WA0124.jpg",
+        "https://i.ibb.co/0jH2PV58/IMG-20250701-WA0116.jpg",
+        "https://i.ibb.co/xqh290XP/IMG-20250701-WA0113.jpg",
+        "https://i.ibb.co/My1LBmMr/IMG-20250701-WA0112.jpg",
+        "https://i.ibb.co/GvV3Crrr/IMG-20250701-WA0110.jpg",
+        "https://i.ibb.co/Xk8pKRM8/IMG-20250701-WA0109.jpg",
+        "https://i.ibb.co/S7HR5f1T/IMG-20250701-WA0095.jpg"
+    ],
+    B: [
+        "https://i.ibb.co/Kj4t9xNS/IMG-20250701-WA0162.jpg",
+        "https://i.ibb.co/zdq4mDc/IMG-20250701-WA0151.jpg",
+        "https://i.ibb.co/gbqWJ7D5/IMG-20250701-WA0152.jpg",
+        "https://i.ibb.co/SD6WfDxn/IMG-20250701-WA0140.jpg",
+        "https://i.ibb.co/G4Yv8P0H/IMG-20250701-WA0131.jpg",
+        "https://i.ibb.co/7NxfGYcY/IMG-20250701-WA0104.jpg"
+    ],
+    A: [
+        "https://i.ibb.co/N63YRrYr/IMG-20250701-WA0163.jpg",
+        "https://i.ibb.co/Xfy6mk94/IMG-20250701-WA0144.jpg"
+    ],
+    S: [
+        "https://i.ibb.co/bgVMhZ8d/IMG-20250701-WA0161.jpg",
+        "https://i.ibb.co/hRBcY6fz/IMG-20250701-WA0143.jpg",
+        "https://i.ibb.co/jZMsSc1S/IMG-20250701-WA0137.jpg",
+        "https://i.ibb.co/TqPtQ30S/IMG-20250701-WA0108.jpg"
+    ]
+};
